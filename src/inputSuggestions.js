b88acdd33b8b633b9a576eb1a5f3ab988adf1be6
lil.void <tsraduu@gmail.com>
2024-04-21 16:55:35 +0100

    input suggestions built, making use of createElement utiliti class so we can add classes
    Seems a bit hard to understand but it's actually easy when it's read out

---
+++ src/inputSuggestions.js
@@ -0,0 +1,47 @@
+import { createElement, getElement } from './utilis';
+
+const suggestionParent = getElement('#suggestions');
+
+export default async function (input) {
+	const query = input.value; // Get input value.
+	suggestionParent.innerHTML = ''; // Clear the previous suggestions.
+
+	// Only fetch suggestions if the input is not empty.
+	if (query) {
+		// await on API for autocomplete suggestions.
+		const suggestions = await getAutocompleteSuggestions(query);
+		// For each suggestion received from the API create a new div, add the suggestions received from API, add event listener and append it to the parent element.
+		suggestions.forEach((suggestion) => {
+			const div = createElement({
+				type: 'div',
+				classes: 'cursor-pointer hover:bg-gray-200 rounded-md mt-2',
+				content: `${suggestion.name}`,
+			});
+			// Add event listener for each suggestion received from API.
+			div.addEventListener('click', () => {
+				handleSuggestion(input, div);
+			});
+			suggestionParent.appendChild(div);
+		});
+	}
+}
+
+function handleSuggestion(input, div) {
+	input.value = div.textContent; // Set the input value to the clicked suggestion
+	suggestionParent.innerHTML = ''; // Clear the suggestions
+}
+
+async function getAutocompleteSuggestions(query) {
+	try {
+		const response = await fetch(
+			`http://api.weatherapi.com/v1/search.json?key=025edeae7fbf45ad949194206241504&q=${query}`,
+		);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = response.json();
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
+}
