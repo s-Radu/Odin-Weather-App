7481f9a38dd8ba35552a350b4d82c09420303026
lil.void <tsraduu@gmail.com>
2024-04-21 16:31:00 +0100

    added comments and input suggestions event listener

--- src/index.js
+++ src/index.js
@@ -1,59 +1,36 @@
+// **** Imports ****
 import toggleDarkMode from './darkMode';
 import processWeatherData from './getData';
+import handleInput from './inputSuggestions';
 import nav from './nav';
 import './style.css';
 import { getElement } from './utilis';
 
+// **** Variables ****
+
 const location = getElement('#location');
 const parentElement = document.body;
 
+// **** Add elements to page ****
+
 parentElement.appendChild(nav());
+
+// **** Event listeners ****
 
 let darkModeToggle = getElement('#toggle');
 darkModeToggle.addEventListener('click', toggleDarkMode);
 
 location.addEventListener('keydown', getLocation);
 
+location.addEventListener('input', function () {
+	handleInput(this);
+});
+
+// **** Functions ****
+
 function getLocation(e) {
 	if (e.key === 'Enter') {
 		processWeatherData(location.value);
 		location.value = '';
-	}
-}
-
-const suggestionsDiv = document.getElementById('suggestions');
-
-location.addEventListener('input', async function () {
-	const query = this.value;
-	suggestionsDiv.innerHTML = ''; // Clear the previous suggestions
-	if (query) {
-		// Only fetch suggestions if the input is not empty
-		const suggestions = await getAutocompleteSuggestions(query);
-		suggestions.forEach((suggestion) => {
-			const div = document.createElement('div');
-			div.textContent = suggestion.name;
-			div.addEventListener('click', function () {
-				location.value = this.textContent; // Set the input value to the clicked suggestion
-				processWeatherData(location.value); // Call the function to trigger the search
-				location.value = '';
-				suggestionsDiv.innerHTML = ''; // Clear the suggestions
-			});
-			suggestionsDiv.appendChild(div);
-		});
-	}
-});
-
-async function getAutocompleteSuggestions(query) {
-	try {
-		const response = await fetch(
-			`http://api.weatherapi.com/v1/search.json?key=025edeae7fbf45ad949194206241504&q=${query}`,
-		);
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
-		}
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
 	}
 }
