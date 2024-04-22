afd490fa11d979c8bc8c8b6890793414b3e8255c
lil.void <tsraduu@gmail.com>
2024-04-22 19:48:01 +0100

    added margin top so it does not interfere with the logo

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ export default function main() {
 	const main = createElement({
 		type: 'main',
 		classes:
-			'min-h-96 dark:text-primary text-black container mx-auto grid grid-cols-1 grid-rows-5',
+			'min-h-96 dark:text-primary text-black container mx-auto grid grid-cols-1 grid-rows-5 mt-20',
 		id: 'main',
 	});
 	return main;
