22d05dddf6c6e62b56065b6eb6a36e5105537c6c
lil.void <tsraduu@gmail.com>
2024-04-22 19:27:59 +0100

    added main.js page

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createElement } from './utilis.js';
+
+export default function main() {
+	const main = createElement({
+		type: 'main',
+		classes:
+			'min-h-96 dark:text-primary text-black container mx-auto grid grid-cols-1 grid-rows-5',
+		id: 'main',
+	});
+	return main;
+}
