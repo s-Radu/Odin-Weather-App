162d2f8322270bf0f45e38a458780243e7f5671e
lil.void <tsraduu@gmail.com>
2024-04-22 19:28:52 +0100

    restructured the variables that hold data
    We changed them so we can pass them to a new function to display the data to the user

--- src/getData.js
+++ src/getData.js
@@ -1,3 +1,10 @@
+import { createElement, getElement } from './utilis';
+let main;
+
+window.addEventListener('DOMContentLoaded', () => {
+	main = getElement('#main');
+});
+
 async function callWeatherAPI(city) {
 	// !Stop forgeting to use FETCH in order to fetch the data from the API!!!!!!!
 
@@ -17,113 +24,35 @@ async function callWeatherAPI(city) {
 }
 
 function extractWeatherData(data) {
-	const { name: city, country } = data.location;
-	const {
-		temp_c: tempC,
-		feelslike_c: feelsLike,
-		humidity,
-		uv,
-		wind_dir,
-		wind_kph,
-		// Now, condition will hold the value of data.current.condition.text, so this way we are using nested destructuring and we avoind making another variable that will make the code even longer
-		condition: { text: condition, icon },
-	} = data.current;
+	main.innerHTML = '';
 
-	const {
-		date: day1Date,
-		astro: { sunrise: day1Sunrise, sunset: day1Sunset },
-		day: {
-			mintemp_c: day1MinTempC,
-			maxtemp_c: day1MaxTempC,
-			avghumidity: day1Humidity,
-			uv: day1UV,
-			avgvis_km: day1Visibility,
-			daily_chance_of_rain: day1RainChances,
-			condition: { text: day1Condition, icon: day1Icon },
-		},
-	} = data.forecast.forecastday[1];
+	const location = { city: data.location.name, country: data.location.country };
+	const currentDay = {
+		tempC: data.current.temp_c,
+		feelsLikeC: data.current.feelslike_c,
+		humidity: data.current.humidity,
+		uv: data.current.uv,
+		windDirection: data.current.wind_dir,
+		windSpeed: data.current.wind_kph,
+		condition: data.current.condition.text,
+		icon: data.current.condition.icon,
+	};
 
-	const {
-		date: day2Date,
-		astro: { sunrise: day2Sunrise, sunset: day2Sunset },
-		day: {
-			mintemp_c: day2MinTempC,
-			maxtemp_c: day2MaxTempC,
-			avghumidity: day2Humidity,
-			uv: day2UV,
-			avgvis_km: day2Visibility,
-			daily_chance_of_rain: day2RainChances,
-			condition: { text: day2Condition, icon: day2Icon },
-		},
-	} = data.forecast.forecastday[2];
-	const {
-		date: day3Date,
-		astro: { sunrise: day3Sunrise, sunset: day3Sunset },
-		day: {
-			mintemp_c: day3MinTempC,
-			maxtemp_c: day3MaxTempC,
-			avghumidity: day3Humidity,
-			uv: day3UV,
-			avgvis_km: day3Visibility,
-			daily_chance_of_rain: day3RainChances,
-			condition: { text: day3Condition, icon: day3Icon },
-		},
-	} = data.forecast.forecastday[3];
+	const forecast = data.forecast.forecastday.map((day) => ({
+		date: day.date,
+		sunrise: day.astro.sunrise,
+		sunset: day.astro.sunset,
+		minTempC: day.day.mintemp_c,
+		maxTempC: day.day.maxtemp_c,
+		avgHumidity: day.day.avghumidity,
+		uv: day.day.uv,
+		avgVisibilityKm: day.day.avgvis_km,
+		chancesOfRaid: day.day.daily_chance_of_rain,
+		condition: day.day.condition.text,
+		icon: day.day.condition.icon,
+	}));
 
-	console.log(`
-	${city},
-	${country},
-	Temp: ${tempC},
-	Feels Like: ${feelsLike},
-	Humidity: ${humidity}
-	UV: ${uv}
-	Wind Direction: ${wind_dir}
-	Wind Speed: ${wind_kph}
-	And it's currently: ${condition}
-	${icon}
-	`);
 
-	console.log(`Forecast:
-	${day1Date}
-	Sunrise: ${day1Sunrise}
-	Sunset: ${day1Sunset}
-	Min Temp: ${day1MinTempC}
-	Max Temp: ${day1MaxTempC}
-	Condition: ${day1Condition}
-	Average humidity: ${day1Humidity}%
-	UV: ${day1UV}
-	Rain chances are: ${day1RainChances}%
-	Visibility: ${day1Visibility} km
-	${day1Icon}
-	`);
-
-	console.log(`Forecast:
-	${day2Date}
-	Sunrise: ${day2Sunrise}
-	Sunset: ${day2Sunset}
-	Min Temp: ${day2MinTempC}
-	Max Temp: ${day2MaxTempC}
-	Condition: ${day2Condition}
-	Average humidity: ${day2Humidity}%
-	UV: ${day2UV}
-	Rain chances are: ${day2RainChances}%
-	Visibility: ${day2Visibility} km
-	${day2Icon}
-	`);
-
-	console.log(`Forecast:
-	${day3Date}
-	Sunrise: ${day3Sunrise}
-	Sunset: ${day3Sunset}
-	Min Temp: ${day3MinTempC}
-	Max Temp: ${day3MaxTempC}
-	Condition: ${day3Condition}
-	Average humidity: ${day3Humidity}%
-	UV: ${day3UV}
-	Rain chances are: ${day3RainChances}%
-	Visibility: ${day3Visibility} km
-	${day3Icon}
-	`);
 }
 
 export default async function processWeatherData(city) {
